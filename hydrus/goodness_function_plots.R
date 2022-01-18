37a106096ee04233c627a54bb5dd3fa6dd7a0bfb
SonjaBauer <sonja.bauer.1@uni-potsdam.de>
2022-01-18 18:38:46 +0100

    added file goodness_function_plots.R

---
+++ hydrus/goodness_function_plots.R
@@ -0,0 +1,90 @@
+# Goodness function and plots for Hydrus model
+#
+#end time for crns data has to be updated!!
+
+
+##### Read CRNS data #####
+crns0 <- read.table("C:/Users/bauers/data/crns/Spo0_df24_hourly.txt", header=T, sep=",")
+crns0$datetime <- as.POSIXct(crns0$datetime)
+crns1 <- read.table("C:/Users/bauers/data/crns/Spo1_df24_hourly.txt", header=T, sep=",")
+crns1$datetime <- as.POSIXct(crns1$datetime)
+crns2 <- read.table("C:/Users/bauers/data/crns/Spo2_df24_hourly.txt", header=T, sep=",")
+crns2$datetime <- as.POSIXct(crns2$datetime)
+
+# rescale neutron counts of the different crns probes
+crns1$cphc <- crns1$cphc * 3.23
+crns0$cphc <- crns0$cphc * 3.23 / 0.9726
+
+crns <- rbind(crns0[,c(1,20,21)], crns1[,c(1,17,18)], 
+              crns2[c((which(crns2$datetime==max(crns1$datetime))+1):which(crns2$datetime=="2021-08-28")),c(1,20,21)])
+
+## Function for computing goodness
+goodness <- function(project.path) {
+  
+  # Display model output #####
+  mod_cosmic <- read.table(paste0(project.path,"/Cosmic.out"), skip=4)
+  mod_flux <- read.table(paste0(project.path,"/T_Level.out"), skip=9, 
+                         nrow=length(readLines(paste0(project.path,"/T_Level.out"))) - 10)
+  mod_obsnode <- read.table(paste0(project.path,"/Obs_Node.out"), skip=11, 
+                            nrow=length(readLines(paste0(project.path,"/Obs_Node.out"))) - 12)
+  
+  names(mod_cosmic) <- c("Time","NFlux")
+  names(mod_flux) <- c("Time","rTop","rRoot","vTop","vRoot","vBot","sum_rTop","sum_rRoot","sum_vTop","sum_vRoot","sum_Bot","hTop","hRoot","hBot",
+                       "RunOff","sum_RunOff","Volume","sum_Infil","sum_Evap","TLevel","Cum_WTrans","SnowLayer","01","02","03","04")
+  names(mod_obsnode) <- c("Time",paste0(c("h_","theta_","Temp_"),c(1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6)))
+  
+  # set datetime for hydrus output
+  mod_cosmic$Time <- seq.POSIXt(as.POSIXct("2020-01-01"),by="hours", length.out=length(mod_cosmic$Time))
+  mod_flux$Time <- seq.POSIXt(as.POSIXct("2020-01-01"),by="hours", length.out=length(mod_cosmic$Time))
+  mod_obsnode$Time <- seq.POSIXt(as.POSIXct("2020-01-01"),by="hours", length.out=length(mod_cosmic$Time))
+  
+  
+  # Gütemaße für Modell
+  # Modellergebnisse für die es auch Daten gibt:
+  mod_cosmic_fit <- mod_cosmic[c(which(mod_cosmic$Time==min(crns0$datetime)):which(mod_cosmic$Time==max(crns0$datetime)),
+                                 which(mod_cosmic$Time==min(crns1$datetime)):which(mod_cosmic$Time=="2021-08-28")),]
+  
+  rmse <- rmse(crns$cphc, mod_cosmic_fit$NFlux)
+  
+  lm.daten.modell <- lm(mod_cosmic_fit$NFlux ~ crns$cphc)
+  r_2 <- summary(lm.daten.modell)$r.squared
+  
+  nse <- NSE(mod_cosmic_fit$NFlux, crns$cphc)
+  
+  # mean of data
+  mean_mod <- mean(mod_cosmic_fit$NFlux)
+  mean_crns <- mean(crns$cphc, na.rm=T)
+  
+  
+  ##### plot model output against data ####
+  #oldpar <- par()
+  par(las=0, mgp=c(2.5,0.7,0), mar=c(2,3.5,1,1))
+  plot(mod_cosmic, type="l", ylim=c(2100,3200))
+  lines(crns0$datetime, crns0$cphc, col=3)
+  lines(crns1$datetime, crns1$cphc, col=4)
+  lines(crns2$datetime, crns2$cphc, col=2)
+  
+  # Plot model fluxes 
+  par(las=1, mgp=c(2.5,0.7,0), mar=c(2,3.5,1,1))
+  plot(mod_flux$Time, mod_flux$sum_vRoot, type="l", ylim=c(-30,30),
+       xlab = "Time", ylab = "Flux [cm]")
+  lines(mod_flux$Time, mod_flux$sum_vTop, col=2)
+  lines(mod_flux$Time, mod_flux$sum_Bot, col=4)
+  legend("topleft",c("Root Water Uptake", "Top Flux", "Bottom Flux"),
+         col=c(1,2,4), lwd=2, cex=0.9)
+  
+  # Plot theta of observation nodes
+  plot(mod_obsnode$Time, mod_obsnode[,3], type="l", ylim=c(0.1,0.4),
+       xlab = "Time", ylab = "Theta")
+  grid()
+  for (i in 2:6) {
+    lines(mod_obsnode$Time, mod_obsnode[,3*i], col=i)
+  }
+  legend("bottomright", c("10cm","20cm","30cm","40cm","60cm","100cm"),
+         col=c(1:6), lwd=2, cex=0.8, bty="n")
+  
+  goods <- c(rmse, r_2, nse)
+
+  return(goods)
+
+}
